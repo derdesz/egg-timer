2ccae790d82d812098459ae698482db514755ca0
derdesz <dora.mailbox@gmail.com>
2021-03-04 08:43:03 +0100

    Finished project

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,15 +1,66 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    
+    @IBOutlet weak var topView: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
 
+        progressView.progress = 0.0
+        secondsPassed = 0
+        topView.text = sender.currentTitle
+        
+        var hardness = sender.currentTitle
+        
+        switch hardness {
+            case "Soft":
+                totalTime = 5
+            case "Medium":
+                totalTime = 420
+            case "Hard":
+                totalTime = 720
+            default:
+                print("None")
+            }
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+    }
+    
+   
+    
+    @objc func timerAction(){
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressView.progress = Float(secondsPassed) / Float(totalTime)
+            
+        } else{
+            timer.invalidate()
+            topView.text = "DONE!"
+            playSound()
+            
+        }
+        
+    }
+    
+ 
+    
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "success", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+
+    }
 
 }
+
